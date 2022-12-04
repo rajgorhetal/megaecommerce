cb3b75e9e747922954788f00401a03aa1404a28c
Hetal Rajgor <hetalrajgorb@gmail.com>
2022-12-04 11:43:15 +0530

    added morgan and express middleware

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import morgan from "morgan";
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//TODO: to configure cors url
+app.use(cors());
+app.use(cookieParser());
+
+//morgan logger
+app.use(morgan("tiny"));
 
 export default app;
